118c0c14699c963ee6b9011421b77b09f817d7cd
sanjayaharshana <sanjayaharasana@gmail.com>
2021-07-30 15:34:28 +0530

    NAVBAR Sections

--- resources/views/frontend/inc/nav.blade.php
+++ resources/views/frontend/inc/nav.blade.php
@@ -9,65 +9,69 @@
 </div>
 @endif
 <!-- Top Bar -->
-<div class="top-navbar bg-white border-bottom border-soft-secondary z-1035" style="background: #ebedf2 !important;color: #080808 !important;">
+<div class="top-navbar bg-white border-bottom border-soft-secondary z-1035" style="background: #ffffff !importantcolor: #080808 !important;">
     <div class="container">
         <div class="row">
             <div class="col-lg-7 col">
                 <ul class="list-inline d-flex justify-content-between justify-content-lg-start mb-0" style="padding-top: 8px;">
-                    @if(get_setting('show_language_switcher') == 'on')
-                    <li class="list-inline-item dropdown mr-3" id="lang-change">
-                        @php
-                            if(Session::has('locale')){
-                                $locale = Session::get('locale', Config::get('app.locale'));
-                            }
-                            else{
-                                $locale = 'en';
-                            }
-                        @endphp
-                        <a href="javascript:void(0)" class="dropdown-toggle text-reset py-2" data-toggle="dropdown" data-display="static">
-                            <img src="{{ static_asset('assets/img/placeholder.jpg') }}" data-src="{{ static_asset('assets/img/flags/'.$locale.'.png') }}" class="mr-2 lazyload" alt="{{ \App\Language::where('code', $locale)->first()->name }}" height="11">
-                            <span class="opacity-60">{{ \App\Language::where('code', $locale)->first()->name }}</span>
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-left">
-                            @foreach (\App\Language::all() as $key => $language)
-                                <li>
-                                    <a href="javascript:void(0)" data-flag="{{ $language->code }}" class="dropdown-item @if($locale == $language) active @endif">
-                                        <img src="{{ static_asset('assets/img/placeholder.jpg') }}" data-src="{{ static_asset('assets/img/flags/'.$language->code.'.png') }}" class="mr-1 lazyload" alt="{{ $language->name }}" height="11">
-                                        <span class="language">{{ $language->name }}</span>
-                                    </a>
-                                </li>
-                            @endforeach
-                        </ul>
-                    </li>
-                    @endif
 
-                    @if(get_setting('show_currency_switcher') == 'on')
-                    <li class="list-inline-item dropdown" id="currency-change">
-                        @php
-                            if(Session::has('currency_code')){
-                                $currency_code = Session::get('currency_code');
-                            }
-                            else{
-                                $currency_code = \App\Currency::findOrFail(get_setting('system_default_currency'))->code;
-                            }
-                        @endphp
-                        <a href="javascript:void(0)" class="dropdown-toggle text-reset py-2 opacity-60" data-toggle="dropdown" data-display="static">
-                            {{ \App\Currency::where('code', $currency_code)->first()->name }} {{ (\App\Currency::where('code', $currency_code)->first()->symbol) }}
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-right dropdown-menu-lg-left">
-                            @foreach (\App\Currency::where('status', 1)->get() as $key => $currency)
-                                <li>
-                                    <a class="dropdown-item @if($currency_code == $currency->code) active @endif" href="javascript:void(0)" data-currency="{{ $currency->code }}">{{ $currency->name }} ({{ $currency->symbol }})</a>
-                                </li>
-                            @endforeach
-                        </ul>
-                    </li>
-                    @endif
                 </ul>
             </div>
 
             <div class="col-5 text-right d-none d-lg-block">
                 <ul class="list-inline mb-0">
+
+                    @if(get_setting('show_language_switcher') == 'on')
+                        <li class="list-inline-item dropdown mr-3" id="lang-change">
+                            @php
+                                if(Session::has('locale')){
+                                    $locale = Session::get('locale', Config::get('app.locale'));
+                                }
+                                else{
+                                    $locale = 'en';
+                                }
+                            @endphp
+                            <a href="javascript:void(0)" class="dropdown-toggle text-reset py-2" data-toggle="dropdown" data-display="static">
+                                <img src="{{ static_asset('assets/img/placeholder.jpg') }}" data-src="{{ static_asset('assets/img/flags/'.$locale.'.png') }}" class="mr-2 lazyload" alt="{{ \App\Language::where('code', $locale)->first()->name }}" height="11">
+                                <span class="opacity-60">{{ \App\Language::where('code', $locale)->first()->name }}</span>
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-left">
+                                @foreach (\App\Language::all() as $key => $language)
+                                    <li>
+                                        <a href="javascript:void(0)" data-flag="{{ $language->code }}" class="dropdown-item @if($locale == $language) active @endif">
+                                            <img src="{{ static_asset('assets/img/placeholder.jpg') }}" data-src="{{ static_asset('assets/img/flags/'.$language->code.'.png') }}" class="mr-1 lazyload" alt="{{ $language->name }}" height="11">
+                                            <span class="language">{{ $language->name }}</span>
+                                        </a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </li>
+                    @endif
+
+                    @if(get_setting('show_currency_switcher') == 'on')
+                        <li class="list-inline-item dropdown" id="currency-change">
+                            @php
+                                if(Session::has('currency_code')){
+                                    $currency_code = Session::get('currency_code');
+                                }
+                                else{
+                                    $currency_code = \App\Currency::findOrFail(get_setting('system_default_currency'))->code;
+                                }
+                            @endphp
+                            <a href="javascript:void(0)" class="dropdown-toggle text-reset py-2 opacity-60" data-toggle="dropdown" data-display="static">
+                                {{ \App\Currency::where('code', $currency_code)->first()->name }} {{ (\App\Currency::where('code', $currency_code)->first()->symbol) }}
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-right dropdown-menu-lg-left">
+                                @foreach (\App\Currency::where('status', 1)->get() as $key => $currency)
+                                    <li>
+                                        <a class="dropdown-item @if($currency_code == $currency->code) active @endif" href="javascript:void(0)" data-currency="{{ $currency->code }}">{{ $currency->name }} ({{ $currency->symbol }})</a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </li>
+                    @endif
+
+
                     @auth
                         @if(isAdmin())
                             <li class="list-inline-item mr-3">
@@ -95,21 +99,24 @@
     </div>
 </div>
 <!-- END Top Bar -->
+@php
+    $header_logo = get_setting('header_logo');
+@endphp
+@if($header_logo != null)
+    <img src="{{ uploaded_asset($header_logo) }}" alt="{{ env('APP_NAME') }}" class="mw-100 h-30px h-md-40px" height="40" style="height: 123px;position: fixed;z-index: 100000;left: 170px;top: 12px;">
+@else
+    <img src="{{ static_asset('assets/img/logo.png') }}" alt="{{ env('APP_NAME') }}" class="mw-100 h-30px h-md-40px" height="40">
+@endif
+
 <header class="@if(get_setting('header_stikcy') == 'on') sticky-top @endif z-1020 bg-white border-bottom shadow-sm">
-    <div class="position-relative logo-bar-area z-1">
+    <div class="position-relative logo-bar-area z-1" style="height: 100px;padding-top: 20px;padding-bottom: 130px;">
+
         <div class="container">
             <div class="d-flex align-items-center">
 
                 <div class="col-auto col-xl-3 pl-0 pr-3 d-flex align-items-center">
                     <a class="d-block py-20px mr-3 ml-0" href="{{ route('home') }}">
-                        @php
-                            $header_logo = get_setting('header_logo');
-                        @endphp
-                        @if($header_logo != null)
-                            <img src="{{ uploaded_asset($header_logo) }}" alt="{{ env('APP_NAME') }}" class="mw-100 h-30px h-md-40px" height="40" style="height: 80px;">
-                        @else
-                            <img src="{{ static_asset('assets/img/logo.png') }}" alt="{{ env('APP_NAME') }}" class="mw-100 h-30px h-md-40px" height="40">
-                        @endif
+                       {{--Currect Logo--}}
                     </a>
 
                     @if(Route::currentRouteName() != 'home')
