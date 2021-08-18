af11a959a3ce5aaedb6cce5b8ce35db15b6e32b9
sanjayaharshana <sanjayaharasana@gmail.com>
2021-08-18 09:44:03 +0530

    Revert "Home Page Features"
    This reverts commit 76bbcf9e164ccc8b3ffc83801febcfa9d03ad5fa.

--- resources/views/frontend/partials/home_page_features.blade.php
+++ resources/views/frontend/partials/home_page_features.blade.php
@@ -1,3 +1,131 @@
 
 
 
+<div class="" style="margin-top: 50px;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body" style="text-align: center">
+                    <h5> {{ translate('Shop by Store') }}</h5><br>
+                        <figure class="c4-izmir c4-border-corners-1 c4-image-zoom-out c4-gradient-bottom" tabindex="0" style="--primary-color: #000000;--secondary-color: #000000;--text-color: #f0f0f0;--border-color: #ffffff;--image-opacity: 60%;width: 380px;">
+                            <img src="{{url('imgs/1.jpg')}}" alt="Sample Image">
+                            <figcaption class="c4-layout-top-left">
+                                <div class="c4-reveal-down">
+                                    <div>
+                                        <h2 style="margin-bottom: 5px;">
+                                            {{ translate('Shop by Store') }}
+                                        </h2>
+                                        <p>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.</p>
+                                    </div>
+                                </div>
+                            </figcaption>
+                        </figure><br>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body" style="text-align: center">
+                        <h5>{{ translate('Shop by Category') }}</h5><br>
+                        <figure class="c4-izmir c4-border-corners-1 c4-image-zoom-out c4-gradient-bottom" tabindex="0" style="--primary-color: #000000;--secondary-color: #000000;--text-color: #f0f0f0;--border-color: #ffffff;--image-opacity: 60%;width: 380px;">
+                            <img src="{{url('imgs/2.jpg')}}" alt="Sample Image">
+                            <figcaption class="c4-layout-top-left">
+                                <div class="c4-reveal-down">
+                                    <div>
+                                        <h2 style="margin-bottom: 5px;">
+                                            {{ translate('Shop by Category') }}
+                                        </h2>
+                                        <p>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.</p>
+                                        <a href="" class="btn btn-primary">Hello Word</a>
+                                    </div>
+                                </div>
+                            </figcaption>
+                        </figure><br>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body" style="text-align: center">
+                        <h5> {{ translate('Ask the Book Finder') }}</h5><br>
+                        <figure class="c4-izmir c4-border-corners-1 c4-image-zoom-out c4-gradient-bottom" tabindex="0" style="--primary-color: #000000;--secondary-color: #000000;--text-color: #f0f0f0;--border-color: #ffffff;--image-opacity: 60%;width: 380px;">
+                            <img src="{{url('imgs/3.jpg')}}" alt="Sample Image">
+                            <figcaption class="c4-layout-top-left">
+                                <div class="c4-reveal-down">
+                                    <div>
+                                        <h2 style="margin-bottom: 5px;">
+                                            {{ translate('Ask the Book Finder') }}
+                                        </h2>
+                                        <p>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.</p>
+                                    </div>
+                                </div>
+                            </figcaption>
+                        </figure><br>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body" style="text-align: center">
+                        <h5>{{ translate('Visit the Book Fair') }}</h5><br>
+                        <figure class="c4-izmir c4-border-corners-1 c4-image-zoom-out c4-gradient-bottom" tabindex="0" style="--primary-color: #000000;--secondary-color: #000000;--text-color: #f0f0f0;--border-color: #ffffff;--image-opacity: 60%;width: 380px;">
+                            <img src="{{url('imgs/4.jpg')}}" alt="Sample Image">
+                            <figcaption class="c4-layout-top-left">
+                                <div class="c4-reveal-down">
+                                    <div>
+                                        <h2 style="margin-bottom: 5px;">
+                                            {{ translate('Visit the Book Fair') }}
+                                        </h2>
+                                        <p>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.</p>
+                                    </div>
+                                </div>
+                            </figcaption>
+                        </figure><br>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body" style="text-align: center">
+                        <h5>{{ translate('Stationary Corner') }}</h5><br>
+                        <figure class="c4-izmir c4-border-corners-1 c4-image-zoom-out c4-gradient-bottom" tabindex="0" style="--primary-color: #000000;--secondary-color: #000000;--text-color: #f0f0f0;--border-color: #ffffff;--image-opacity: 60%;width: 380px;">
+                            <img src="{{url('imgs/5.jpg')}}" alt="Sample Image">
+                            <figcaption class="c4-layout-top-left">
+                                <div class="c4-reveal-down">
+                                    <div>
+                                        <h2 style="margin-bottom: 5px;">
+                                            {{ translate('Stationary Corner') }}
+                                        </h2>
+                                        <p>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.</p>
+                                    </div>
+                                </div>
+                            </figcaption>
+                        </figure><br>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body" style="text-align: center">
+                        <h5>{{ translate('Gift Cards') }}</h5><br>
+                        <figure class="c4-izmir c4-border-corners-1 c4-image-zoom-out c4-gradient-bottom" tabindex="0" style="--primary-color: #000000;--secondary-color: #000000;--text-color: #f0f0f0;--border-color: #ffffff;--image-opacity: 60%;width: 380px;">
+                            <img src="{{url('imgs/6.jpg')}}" alt="Sample Image">
+                            <figcaption class="c4-layout-top-left">
+                                <div class="c4-reveal-down">
+                                    <div>
+                                        <h2 style="margin-bottom: 5px;">
+                                            {{ translate('Gift Cards') }}
+                                        </h2>
+                                        <p>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.</p>
+                                    </div>
+                                </div>
+                            </figcaption>
+                        </figure><br>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
