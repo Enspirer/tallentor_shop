ef336abcf1329277aac4f71442c557d9dad2a8b6
nihsaan7 <nihsaan.enspirer@gmail.com>
2021-09-18 11:15:43 +0530

    book finder mobile responsive

--- resources/views/frontend/book_finder.blade.php
+++ resources/views/frontend/book_finder.blade.php
@@ -21,80 +21,63 @@
     </div>
 </section>
 
-<div class="container-fluid">
+
 <section class="mb-4">
-    
-    <div style="padding: 25px; background: url('{{url('assets/img/Book_finder.svg')}}'); width:100%; background-repeat: no-repeat; background-size: auto;" >
-    
-        
+    <div class="container-fluid">
 
-        <div class="row">
-            <div class="col-4" style="margin: 120px; 0 0 0;">
+        <div style="padding: 25px; background: url('{{url('assets/img/Book_finder.svg')}}'); height:600px; width:100%; background-repeat: no-repeat; background-size: cover;" class="img-fluid" alt="Responsive image">
+
+            <div class="row ml-2 mt-2">
             
-            <form action="{{ route('book_finder_request.store') }}" method="post">
-                {{csrf_field()}}
-                    <div class="text-center mb-5">
-                        <h4><b>Book Finder Request</b></h4>                        
-                    </div>
-                    <div>
-
-                        <div class="form-group row">
-                            <!-- <label class="col-md-3 col-form-label">
-                                {{translate('Book Name :')}}
-                                <span class="text-danger">*</span>
-                            </label> -->
-                            <div class="col-md-12">
-                                <input type="text" id="book_name" name="book_name" class="form-control p-4" style="box-shadow: 0 3px 10px rgb(0 0 0 / 0.2)" placeholder="Book Name" required>
-                            </div>
+                <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-5  px-5 mt-5">
+                    <form action="{{ route('book_finder_request.store') }}" method="post">
+                    {{csrf_field()}}
+                        <div class="text-center mb-5">
+                            <h4><b>Book Finder Request</b></h4>                        
                         </div>
-                        <div class="form-group row">
-                            <!-- <label class="col-md-3 col-form-label">
-                                {{translate('Author Name :')}}
-                                <span class="text-danger">*</span>
-                            </label> -->
-                            <div class="col-md-12">
-                                <input type="text" id="author_name" name="author_name" class="form-control p-4" style="box-shadow: 0 3px 10px rgb(0 0 0 / 0.2)" placeholder="Author Name" required>
-                            </div>
-                        </div> 
-                        <div class="form-group row">
-                            <!-- <label class="col-md-3 col-form-label">
-                                {{translate('Publisher Name :')}}
-                                <span class="text-danger">*</span>
-                            </label> -->
-                            <div class="col-md-12">
-                                <input type="text" id="publisher_name" name="publisher_name" class="form-control p-4" style="box-shadow: 0 3px 10px rgb(0 0 0 / 0.2)" placeholder="Publisher Name" required>
-                            </div>
-                        </div> 
-                        <div class="form-group row">
-                            <!-- <label class="col-md-3 col-form-label">
-                                {{translate('Book Type :')}}
-                                <span class="text-danger">*</span>
-                            </label> -->
-                            <div class="col-md-12">
-                                <input type="text" id="book_type" name="book_type" class="form-control p-4" style="box-shadow: 0 3px 10px rgb(0 0 0 / 0.2)" placeholder="Book Type" required>
-                            </div>
-                        </div>  
 
+                        <div class="">
 
-                    </div>
-                    <div class="mt-4">
-                        <button type="button" class="btn btn-light mr-3 px-4 bg-white" style="border-radius: 10px; border: 2px solid red;" data-dismiss="modal"><b>Close</b></button>
-                        <button type="submit" class="btn btn-primary px-4" style="border-radius: 10px;">Send Request</button>
-                    </div>
-                </form>  
+                            <div class="form-group row">
+                                <div class="col-md-12">
+                                    <input type="text" id="book_name" name="book_name" class="form-control p-4" style="box-shadow: 0 3px 10px rgb(0 0 0 / 0.2)" placeholder="Book Name" required>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <div class="col-md-12">
+                                    <input type="text" id="author_name" name="author_name" class="form-control p-4" style="box-shadow: 0 3px 10px rgb(0 0 0 / 0.2)" placeholder="Author Name" required>
+                                </div>
+                            </div> 
+                            <div class="form-group row">
+                                <div class="col-md-12">
+                                    <input type="text" id="publisher_name" name="publisher_name" class="form-control p-4" style="box-shadow: 0 3px 10px rgb(0 0 0 / 0.2)" placeholder="Publisher Name" required>
+                                </div>
+                            </div> 
+                            <div class="form-group row">
+                                <div class="col-md-12">
+                                    <input type="text" id="book_type" name="book_type" class="form-control p-4" style="box-shadow: 0 3px 10px rgb(0 0 0 / 0.2)" placeholder="Book Type" required>
+                                </div>
+                            </div>  
 
-            </div>
-            <div class="col-4">
+                        </div>
+                        <div class="mt-4">
+                            <a href="{{ url('/') }}" class="btn btn-light mr-3 px-4 bg-white" style="border-radius: 10px; border: 2px solid red;"><b>Back</b></a>
+                            <button type="submit" class="btn btn-primary px-4" style="border-radius: 10px;">Send Request</button>
+                        </div>
+                    </form>
+                </div>
             
-            </div>
-            <div class="col-4">
+                <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-4">            
+                </div>
             
+                <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-2">                    
+                </div>
+
             </div>
-        </div>
-    
+        </div>  
+
     </div>
-    
 </section>
-</div>
+
 
 @endsection
